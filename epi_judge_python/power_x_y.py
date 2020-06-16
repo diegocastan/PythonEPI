1d0630399e159c04f60bea012d67a4e0c8b392b7
diegocastan <dcastan@gmail.com>
2020-06-16 11:08:04 -0400

    Add power_x_y.py

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,10 +2,17 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # PROBLEM 4.7
+
+    result, power = 1.0, y
+    if y<0:
+        power, x = -power, 1.0/x
+    while power:
+        if power & 1:
+            result *= x
+        x, power = x * x, power >> 1
+    return result
 
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
-                                        power))
+    exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv', power))

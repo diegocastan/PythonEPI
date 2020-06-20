131b63a76b21cc7834887455f07684dad62b8c01
diegocastan <dcastan@gmail.com>
2020-06-20 13:25:00 -0400

    Add string_integer_interconversion.py (without srt or int functions)

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -4,8 +4,7 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
 
 
 if __name__ == '__main__':
--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,55 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+
+    import math
+
+    if x == 0:
+        return '0'
+
+    res = ''
+    if x < 0:
+        res = '-'
+        xa = abs(x)
+    else:
+        xa = x
+
+    base = 1
+    while base <= xa:
+        base = base * 10
+
+    while base > 1:
+        base = base/10
+        ds = '%d' % (math.floor(xa/base))
+        res = ''.join([res,ds])
+        xa = xa - math.floor(xa/base)*base
+
+    return res
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    dicdig = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+    l = len(s)
+    neg = False
+    ini = 0
+    if s[0] == '+':
+        ini = 1
+    elif s[0] == '-':
+        ini = 1
+        neg = True
+    else:
+        pass
+
+    res = 0
+    for i in range(ini,l):
+        res = 10*res+dicdig[s[i]]
+
+    if neg:
+        return -1*res
+    else:
+        return res
+
 
 
 def wrapper(x, s):

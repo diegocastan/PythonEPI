43b550ebe20dc23285dfcd6f5ee0dfd263a3a853
diegocastan <dcastan@gmail.com>
2020-06-22 07:52:04 -0400

    Add replace_and_remove.py

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,9 +6,40 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
 
+    # EXTREMELY SLOW SOLUTION [ O(n**2) ] because we are resizing the array for each element
+    # i = 0
+    # while i < len(s):
+    #     if s[i] == 'a':
+    #         s.pop(i)
+    #         s.insert(i,'d')
+    #         s.insert(i, 'd')
+    #     if s[i] == 'b':
+    #         s.pop(i)
+    # return 0
+
+    # First remove the b's and count the a's
+    write_idx, a_count = 0, 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+
+    # Backward iteration: replace 'a's with 'dd's starting from the end.
+    cur_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+    while cur_idx >= 0:
+        if s[cur_idx] == 'a':
+            s[write_idx - 1:write_idx + 1] = 'dd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[cur_idx]
+            write_idx -= 1
+        cur_idx -= 1
+    return final_size
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
